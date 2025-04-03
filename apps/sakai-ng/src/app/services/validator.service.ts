d896be1c424f38bce202e28b5e0d23d2b6deb362
Francis Beltre <francis.beltre.t@gmail.com>
2025-04-03 16:04:33 -0400

    Password Recovery

--- apps/sakai-ng/src/app/services/validator.service.ts
+++ apps/sakai-ng/src/app/services/validator.service.ts
@@ -9,7 +9,8 @@ export class ValidatorService {
   public FullnamePattern: string = '[a-zA-Zñáéíóú]+(?: [a-zA-Zñáéíóú ]+){1,}';
   public floatPattern: string = '[0-9]*(\.[0-9]+)?';
   public emailPattern: string = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}';
-  public cedrncPattern: string = '^(?:[0-9]{9})$|^(?:[0-9]{11})$'
+  public cedrncPattern: string = '^(?:[0-9]{9})$|^(?:[0-9]{11})$';
+  public passwordPattern: RegExp = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}/;
 
   public isValidField(form: FormGroup, field: string): boolean | null {
     return form.controls[field].errors && form.controls[field].touched;
@@ -66,6 +67,9 @@ export class ValidatorService {
 
             case `${this.cedrncPattern}`:
               return 'RNC o CEDULA invalida';
+
+            case `${this.passwordPattern}`:
+              return 'Formato de contraseña invalido'
 
             default:
               break;
