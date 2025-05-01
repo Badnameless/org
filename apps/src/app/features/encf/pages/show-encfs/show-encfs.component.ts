0109d97e477d6674171be4fd4fc0272c4da3c075
Francis Beltre <francis.beltre.t@gmail.com>
2025-05-01 16:15:16 -0400

    Datagrid Filter Fixed

--- apps/src/app/features/encf/pages/show-encfs/show-encfs.component.ts
+++ apps/src/app/features/encf/pages/show-encfs/show-encfs.component.ts
@@ -14,6 +14,7 @@ import { Ncf } from '../../interfaces/encf';
 export class ShowEncfsComponent implements OnInit {
   columns: Column[] = [];
   Encfs!: Ncf[];
+  filterFields: string[] = ['transncf_encf', 'transncf_status', 'tenant_name', 'tenant_cedrnc', 'transncf_rnccomprador']
 
   constructor(protected encfService: EncfService) { }
 
