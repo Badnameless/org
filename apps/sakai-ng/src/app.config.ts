c1b280732a29539feec8e22a2e4b8c2ff5f0a900
Francis Beltre <francis.beltre.t@gmail.com>
2025-05-17 13:51:40 -0400

    Implementacion de notificaciones por email

--- apps/sakai-ng/src/app.config.ts
+++ apps/sakai-ng/src/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeng/themes/aura';
@@ -7,6 +7,10 @@ import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
 import { authInterceptor } from './app/features/auth/interceptors/auth-interceptor.interceptor';
 import { provideAngularSvgIcon } from 'angular-svg-icon';
+import localeEsDo from '@angular/common/locales/es-DO';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsDo);
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -17,6 +21,8 @@ export const appConfig: ApplicationConfig = {
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
         providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
-        provideAngularSvgIcon()
+        provideAngularSvgIcon(),
+        { provide: LOCALE_ID, useValue: 'es-DO' }  // or any locale code
+
     ]
 };
