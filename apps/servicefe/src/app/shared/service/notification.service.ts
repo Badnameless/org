1234b0eebfbc61dba66f8ada7002e4baeda5de5e
Francis Beltre <francis.beltre.t@gmail.com>
2025-05-20 10:57:10 -0400

    Real time notifications

--- apps/servicefe/src/app/shared/service/notification.service.ts
+++ apps/servicefe/src/app/shared/service/notification.service.ts
@@ -5,19 +5,30 @@ import { Token } from '../../features/auth/interfaces/token';
 import { Notificacion } from '../interfaces/Notificacion';
 import { lastValueFrom, map } from 'rxjs';
 import { CacheService } from '../../services/cache.service';
+import { WebSocketService } from './web-socket.service';
+import { User } from '../../features/auth/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor(private http: HttpClient,
-    private httpService: HttpService,
-    private cache: CacheService
-  ) { }
-
+  private user: User = JSON.parse(localStorage.getItem('user')!);
   private cacheKey = 'api/notifications';
   private ttl: number = 1000 * 60 * 5;
+
+  constructor(private http: HttpClient,
+    private httpService: HttpService,
+    private cache: CacheService,
+    private wsService: WebSocketService
+  ) {
+
+    wsService.listenToUserNotifications(this.user.user_id, async (notificacion) => {
+      let cached: Promise<Notificacion[]> = await this.cache.getCache(this.cacheKey, this.ttl);
+      (await cached).push(notificacion);
+      cache.setCache(this.cacheKey, cached);
+    })
+  }
 
   async getNotifications(): Promise<Notificacion[]> {
     const token: Token = JSON.parse(localStorage.getItem('token')!);
@@ -27,23 +38,23 @@ export class NotificationService {
 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token.access_token}`);
 
-    const data: Notificacion[] = await lastValueFrom(this.http.get<Notificacion[]>(`${this.httpService.API_URL}/get/notifications`, { headers }))
+    const data: Notificacion[] = await lastValueFrom(this.http.get<Notificacion[]>(`${this.httpService.API_URL}/get/notifications`, { headers }));
 
     this.cache.setCache(this.cacheKey, data);
 
     return data;
   }
 
-  async readNotifications(notificaciones: number[]): Promise<Notificacion[]>{
+  async readNotifications(notificaciones: number[]): Promise<Notificacion[]> {
     const token: Token = JSON.parse(localStorage.getItem('token')!);
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token.access_token}`);
 
-    let data: Notificacion[] = await lastValueFrom(this.http.post<Notificacion[]>(`${this.httpService.API_URL}/read/notifications`, { notificaciones } , { headers }).pipe(
-          map(notificacion => notificacion.map(notificacion => ({
-            ...notificacion,
-            created_at: new Date(notificacion.created_at)
-          })))
-        ))
+    let data: Notificacion[] = await lastValueFrom(this.http.post<Notificacion[]>(`${this.httpService.API_URL}/read/notifications`, { notificaciones }, { headers }).pipe(
+      map(notificacion => notificacion.map(notificacion => ({
+        ...notificacion,
+        created_at: new Date(notificacion.created_at)
+      })))
+    ))
 
     this.cache.setCache(this.cacheKey, data);
 
