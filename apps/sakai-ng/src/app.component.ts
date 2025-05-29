d97134f381cff85481d593e5c96c29940a994fb5
Francis Beltre <francis.beltre.t@gmail.com>
2025-05-29 10:07:54 -0400

    Token expiration fixed

--- apps/sakai-ng/src/app.component.ts
+++ apps/sakai-ng/src/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, Injector } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Token } from './app/features/auth/interfaces/token';
+import { AuthService } from './app/features/auth/services/auth.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterModule],
-    template: `<router-outlet></router-outlet>`
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule],
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {}
+export class AppComponent {
+
+  public token!: Token | null;
+
+  constructor(private authService: AuthService) {
+    effect(() => {
+      this.token = this.authService.exposedToken()
+      const tokenFromStorage = localStorage.getItem('token');
+
+      if (!this.token || !tokenFromStorage) return;
+
+      this.setTokenRefreshingInterval(this.token)
+    })
+  }
+
+  setTokenRefreshingInterval(token: Token | null) {
+    if (!token) return;
+
+    let tokenDate = new Date(token.timestamp)
+    let invalidationDate = tokenDate.getTime() + (55 * 60 * 1000)
+    let invalidationTime = invalidationDate - new Date().getTime()
+
+    setTimeout(async () => {
+      await lastValueFrom(this.authService.refreshToken(token))
+    }, invalidationTime);
+  }
+}
--- apps/sakai-ng/src/app/shared/interfaces/Notificacion.ts
+++ apps/sakai-ng/src/app/shared/interfaces/Notificacion.ts
@@ -7,4 +7,19 @@ export interface Notificacion {
   created_at:             Date;
   updated_at:             Date;
   deleted_at:             Date;
+  tipo_notificacion:      TipoNotificacion;
+}
+
+export interface TipoNotificacion {
+  tipoNotificaciones_id:   number;
+  tipoNotificaciones_code: number;
+  tipoNotificaciones_name: TipoNotificacionesName;
+  created_at:              Date;
+  updated_at:              Date;
+}
+
+export enum TipoNotificacionesName {
+  Personalizado = "Nuevo Mensaje",
+  PlanAgotado = "Plan agotado",
+  RechazoDGII = "Rechazo DGII",
 }
