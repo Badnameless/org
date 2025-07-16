fee1cbea3ef216b5389598a01bd3aba6a288e81e
Dewinson <dewinsoncruz90@gmail.com>
2025-07-16 16:03:49 -0400

    feat: Display Service Status

--- apps/src/app/features/servicesStatus/interfaces/interface.ts
+++ apps/src/app/features/servicesStatus/interfaces/interface.ts
@@ -1,10 +1,10 @@
 export interface ServiceStatus {
-  Id: number;
-  TenantId: number;
-  IP: string;
-  Hostname: string;
-  LastConnection: Date;
-  Status: string;
-  OsfInfo: string;
+  servicefe_id:        number;
+  servicefe_tenantrnc: string;
+  servicefe_ip:        string;
+  servicefe_hostname:  string;
+  servicefe_status:    boolean;
+  servicefe_osinfo:    string;
+  servicefe_lastconn:  Date;
 }
 
