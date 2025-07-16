cf8f14b4d1daf65d80207d3abc26fc422409595d
Dewinson <dewinsoncruz90@gmail.com>
2025-07-16 10:26:04 -0400

    Visual_DataGrid_filters

--- apps/src/app/features/servicesStatus/pages/status/status.component.ts
+++ apps/src/app/features/servicesStatus/pages/status/status.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataGridComponent } from '../../../../shared/component/data-grid/data-grid.component';
 import { Column } from '../../../../shared/component/data-grid/interfaces/column';
+import { ServiceStatus } from '../../interfaces/interface';
 
 @Component({
   selector: 'app-status',
@@ -9,8 +10,70 @@ import { Column } from '../../../../shared/component/data-grid/interfaces/column
   styleUrl: './status.component.scss'
 })
 export class StatusComponent {
-  columns: Column[] = [];
- 
+  columns: Column[] = [
+    {
+      name: 'id',
+      field: 'Id',
+      type: 'hidden'
+    },
+    {
+      name: 'Hostname',
+      field: 'Hostname',
+      type: 'text'
+    },
+    {
+      name: 'IP',
+      field: 'IP',
+      type: 'text'
+    },
+
+    {
+      name: 'LastConnection',
+      field: 'LastConnection',
+      type: 'date'
+    },
+    {
+      name: 'Status',
+      field: 'Status',
+      type: 'text'
+    },
+    {
+      name: 'OsfInfo',
+      field: 'OsfInfo',
+      type: 'text'
+    },
+    {
+      name: 'TenantId',
+      field: 'TenantId',
+      type: 'text'
+    },
+
+  ];
+
+  filterFields: string[] = ['IP', 'Hostname', 'Status', 'OsfInfo'];
+
+
+  data: ServiceStatus[] = [
+    {
+      Id: 1,
+      TenantId: 1,
+      IP: '192.168.1.1',
+      Hostname: 'localhost',
+      LastConnection: new Date(),
+      Status: 'Online',
+      OsfInfo: 'OSF Info'
+    },
+    {
+      Id: 2,
+      TenantId: 2,
+      IP: '192.168.1.2',
+      Hostname: 'localhost',
+      LastConnection: new Date(),
+      Status: 'Offline',
+      OsfInfo: 'OSF Info'
+    }
+  ];
+
 
 
 }
